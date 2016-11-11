5aff7af800bf04c88acd67586e9f8c0958ac839c
Ritesh goel <riteshgoel11@gmail.com>
2016-11-11 22:36:27 +0530

    Added model class and parsed json weather data

---
+++ app/src/main/java/com/rg/howstheweather/CurrentWeather.java
@@ -0,0 +1,122 @@
+package com.rg.howstheweather;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Ritesh Goel on 11-11-2016.
+ */
+
+public class CurrentWeather {
+    private String mIcon;
+    private String mSummary;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mTimezone;
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public int getIconId(){
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a");
+        dateFormat.setTimeZone(TimeZone.getTimeZone(getTimezone()));
+        Date date = new Date(getTime() * 1000);
+        String timeString = dateFormat.format(date);
+        return timeString;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+}
--- app/src/main/java/com/rg/howstheweather/MainActivity.java
+++ app/src/main/java/com/rg/howstheweather/MainActivity.java
@@ -1,9 +1,15 @@
 package com.rg.howstheweather;
 
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -18,37 +24,70 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private final String forcastUrl = "https://api.darksky.net/forecast/";
     private final String apiKey = "e833e57923fb9fc9479e15ba50e4843f";
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(getForecastUrl(37.8267, -122.4233))
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(getForecastUrl(37.8267, -122.4233))
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
 
-            @Override
-            public void onFailure(Call call, IOException e) {
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG, response.body().string());
-                if (response.isSuccessful()) {
-
-                } else {
+                @Override
+                public void onFailure(Call call, IOException e) {
                     showErrorMessage();
                 }
-            }
-        });
-//        try {
-//            Response response = call.execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        try {
+                            mCurrentWeather = getCurrentWeather(jsonData);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "Network unavailable!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private CurrentWeather getCurrentWeather(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather weather = new CurrentWeather();
+        weather.setHumidity(currently.getDouble("humidity"));
+        weather.setTime(currently.getLong("time"));
+        weather.setTemperature(currently.getDouble("temperature"));
+        weather.setIcon(currently.getString("icon"));
+        weather.setPrecipChance(currently.getDouble("precipProbability"));
+        weather.setSummary(currently.getString("summary"));
+        weather.setTimezone(timezone);
+
+        Log.d(TAG,weather.getFormattedTime());
+
+        return weather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            return true;
+        }
+        return false;
     }
 
     public void showErrorMessage() {
